b41f23ba7efcfa77e51a9926b09bdd1e8fe4a517
Ben <ben.valentim@gmail.com>
2024-04-05 11:54:43 -0300

    fix: wallet connect

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
+         Metamask RainbowKit Login
         </p>
         <div>
           <a
@@ -16,17 +15,10 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            By: {"Quasarway"}
           </a>
         </div>
+        <ConnectButton />
       </div>
 
       <div className={styles.center}>
